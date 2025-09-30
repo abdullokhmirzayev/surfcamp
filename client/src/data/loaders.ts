76a0aecd7c4d52237d7a71d1da30a77853fdd6bb
abdullokhmirzayev <abdullohmirzayev7571@gmail.com>
2025-09-30 12:38:01 +0500

    integration dynamic page with next15 client

--- client/src/data/loaders.ts
+++ client/src/data/loaders.ts
@@ -33,12 +33,58 @@ const homePageQuery = qs.stringify({
 		},
 	},
 })
+const BASE_URL = getStrapiUrl()
 
 export async function getHomePage() {
 	const path = '/api/home-page'
-	const BASE_URl = getStrapiUrl()
-	const url = new URL(path, BASE_URl)
+	const url = new URL(path, BASE_URL)
 	url.search = homePageQuery
 
 	return await fetchAPI(url.href, { method: 'GET' })
+}
+
+const pageBySlugQuery = (slug: string) => qs.stringify(
+  {
+    filters: {
+      slug: {
+        $eq: slug,
+      },
+    },
+    populate: {
+      blocks: {
+        on: {
+          "blocks.hero-section": {
+            populate: {
+              image: {
+                fields: ["url", "alternativeText"],
+              },
+              logo: {
+                populate: {
+                  image: {
+                    fields: ["url", "alternativeText"],
+                  },
+                },
+              },
+              cta: true,
+            },
+          },
+          "blocks.info-block": {
+            populate: {
+              image: {
+                fields: ["url", "alternativeText"],
+              },
+              cta: true,
+            },
+          },
+        },
+      },
+    },
+  },
+);
+
+export async function getPageBySlug(slug: string) {
+  const path = "/api/pages";
+  const url = new URL(path, BASE_URL);
+  url.search = pageBySlugQuery(slug);
+  return await fetchAPI(url.href, { method: "GET" });
 }
